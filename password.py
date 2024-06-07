e89119e59c3865380e78bde9101a24b90215df07
yjlc-pc <yjlc.18979607766@outlook.com>
2024-06-07 14:28:36 +0800

    created API "create_password".

--- password.py
+++ password.py
@@ -1,15 +1,19 @@
 import random
 import base64
-LETTERS = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"] # password's all letter
-NUMBERS = [1,2,3,4,5,6,7,8,9] # password's all number
-OTHERS = ["`","-","=","[","]",'\\',";","'",".",",","/","~","!","@","#","$","%","^","&","*","(",")","_","+","{","}","|",":","\"","<",">","?"] # password's all fuhao
+
+LETTERS = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c",
+           "v", "b", "n", "m"]  # password's all letter
+NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9]  # password's all number
+OTHERS = ["`", "-", "=", "[", "]", '\\', ";", "'", ".", ",", "/", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
+          "_", "+", "{", "}", "|", ":", "\"", "<", ">", "?"]  # password's all fuhao
+
 
 def create_password(byte: int):
     password = ""
-    letter_byte = random.randrange(0,byte)
-    number_byte = random.randrange(0,byte-letter_byte)
-    other_byte = byte-letter_byte-number_byte
-    print(letter_byte,number_byte,other_byte)
+    letter_byte = random.randrange(0, byte)
+    number_byte = random.randrange(0, byte - letter_byte)
+    other_byte = byte - letter_byte - number_byte
+    print(letter_byte, number_byte, other_byte)
     for _ in range(letter_byte):
         password += random.choice(LETTERS)
     for _ in range(number_byte):
@@ -19,5 +23,3 @@ def create_password(byte: int):
     return password
 
 
-
-print(create_password(9))
